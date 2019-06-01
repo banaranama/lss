cf8b877c81e367a84246134d9951f9dea9eb040e
banaranama <49350897+banaranama@users.noreply.github.com>
2019-06-01 14:01:09 +0200

    Update ShareAlliancePost_NonSe.user.js

--- ShareAlliancePost_NonSe.user.js
+++ ShareAlliancePost_NonSe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ShareAlliancePost_NonSe
 // @namespace    Leitstellenspiel
-// @version      10.3.7a
+// @version      10.3.8
 // @author       NewEarth, x_Freya_x, jalibu (Original), JuMaHo (Original)
 // @include      https://www.leitstellenspiel.de/missions/*
 // @grant        GM_setValue
@@ -66,7 +66,7 @@
     const defaultMessages = ['%ESZ% - %ADDRESS% - %CRE% - spielt fair, frei zum verdienen.', // Default
                              '%ESZ% - %CRE% - 🚒 RD für %PATIENTS_LEFT% Patienten in %ADDRESS% benötigt.',
                              '%ESZ% - %CRE% - Weitere Kräfte in %ADDRESS% benötigt.',
-                             '%ESZ% - %CRE% - Weitere Kräfte in %ADDRESS% benötigt, RD frei - %PATIENTS_LEFT% Patienten 🚒.',
+                             '%ESZ% - %CRE% - Weitere Kräfte und RD für %PATIENTS_LEFT% Patienten 🚒 benötigt. %ADDRESS%',
                       '[EVENT] %ESZ% - BITTE offen lassen bis %MY_CUSTOM_TIME3% !!!',
                       '%ADDRESS% - %FRE0%',
                       '%ESZ% - %ADDRESS% - %FRE%',
@@ -275,7 +275,7 @@
     const transformMessages = () => {
         try {
 
-            const vers = '(10.3.7a NonSend)';
+            const vers = '(10.3.8 NonSend)';
 
             var creds, cstr;
 
